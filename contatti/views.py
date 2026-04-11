fc6c73af3bed785bba6e421c5e18bea818b8adb6
ardenzio <gourmetmolise@gmail.com>
2026-04-11 15:46:30 +0200

    feat: mail with messages

--- contatti/views.py
+++ contatti/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-
+from django.contrib import messages
 from casamilano import settings
 from contatti.forms import ContactForm
 from contatti.mail_utils import send_email
@@ -11,14 +11,20 @@ def contatti(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             print("Form received")
-            name = form.cleaned_data["name"]
-            email = form.cleaned_data["email"]
-            subject = form.cleaned_data["subject"]
-            message = form.cleaned_data["message"]
-            email_handler(form.cleaned_data, request)
-            #messages.success(request, "Messaggio inviato correttamente.")
+            try:
+                email_handler(form.cleaned_data, request)
+                messages.success(request, "Abbiamo ricevuto il tuo messaggio. Ti risponderemo presto. Se non vedi la nostra mail, per favore controlla nello spam. Grazie per averci contattato!!")
+            except Exception:
+                messages.error(
+                    request,
+                    "Si è verificato un errore durante l'invio. Riprova più tardi."
+                )
             return redirect("contatti:contatti")
         else:
+            messages.warning(
+                request,
+                "Controlla i campi del form e riprova."
+            )
             print("Form is not valid")
 
     else:
@@ -30,6 +36,7 @@ def contatti(request):
 
 def email_handler(form_data, request):
     print("Email handler")
+    base_uri = request.build_absolute_uri("/").rstrip("/")
     template = 'mails/mail_template.html'
     print(settings.CONTACT_RECEIVER_EMAIL)
     context = {
@@ -39,10 +46,21 @@ def email_handler(form_data, request):
         'name': form_data['name'],
         'email': form_data['email'],
         'message': form_data['message'],
-        'base_uri': request.build_absolute_uri("/").rstrip("/"),
+        'base_uri': base_uri,
     }
 
     send_email(template, context)
+
+    send_email(
+        html_template='mails/reply_mail_template.html',
+        context={
+            'subject': "Grazie per averci contattato",
+            'to_email': form_data['email'],
+            'name': form_data['name'],
+            'message': form_data['message'],
+            'base_uri': base_uri,
+        }
+    )
 
 
 
