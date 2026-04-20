d1c885b505bfe8b132d95b936250fac1456fff82
ardenzio <gourmetmolise@gmail.com>
2026-04-20 10:11:38 +0200

    fix: allow droplet host in local_settings

--- casamilano/local_settings.py
+++ casamilano/local_settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY","")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost","167.71.52.149"]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
